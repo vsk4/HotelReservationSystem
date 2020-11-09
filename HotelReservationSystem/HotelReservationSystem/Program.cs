4bb40734311d04fde429bd3aeee4d66e46687aa4
vsk4 <saikumarvadla4@gmail.com>
2020-11-09 13:29:42 +0530

    [VSK]: Added UC2 to find cheapest hotel

--- HotelReservationSystem/HotelReservationSystem/Program.cs
+++ HotelReservationSystem/HotelReservationSystem/Program.cs
@@ -11,6 +11,10 @@ namespace HotelReservationSystem
             hotelsManager.AddHotel(new Hotel("Lakewood", 110));
             hotelsManager.AddHotel(new Hotel("Bridgewood", 150));
             hotelsManager.AddHotel(new Hotel("Ridgewood", 220));
+            string[] dates = "01Jan2019,11Dec2020".Split(",");
+            Hotel cheapestHotel = hotelsManager.FindCheapestHotel(dates);
+            Console.WriteLine("Cheapest Hotel for given dates: " + cheapestHotel.hotelName);
+            Console.WriteLine("Total Rate: " + cheapestHotel.regularRates * dates.Length);
         }
     }
 }
